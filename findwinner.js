53e52a89473137511c9de22f7a8828ebe92fdfd7
Leonardo A <brinfo.leo@gmail.com>
2021-11-04 10:17:33 +0000

    including the diagonal sum

--- findwinner.js
+++ findwinner.js
@@ -1,6 +1,6 @@
 findWinner = arrayBoard => {
     //board = r0,r1,r2,c0,c1,c2,d0,d1
-    let board = [0,0,0,0,0,0,0,0];
+    let board = [0, 0, 0, 0, 0, 0, 0, 0];
 
     for (let row = 0; row < arrayBoard.length; row++) {
         for (let col = 0; col < arrayBoard[row].length; col++) {
@@ -15,13 +15,18 @@ findWinner = arrayBoard => {
                     arrayBoard[row][col] = 0;
             }
             //sum row
-            board[row] += arrayBoard[row][col]
+            board[row] += arrayBoard[row][col];
             //sum col
-            board[row + 3] += arrayBoard[row][col]
+            board[row + 3] += arrayBoard[row][col];
+
+            //sum diagonal
+
 
         }
 
     }
+    board[6] = board[0][0] + board[1][1] + board[2][2];
+    board[7] = board[0][2] + board[1][1] + board[2][0];
     console.log(board);
     return '0';
 }
